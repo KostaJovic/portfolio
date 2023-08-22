a7ba454166fcbbb7fd020f118f2991d3f9dc370b
KostaJovic <95864701+KostaJovic@users.noreply.github.com>
2023-08-22 19:06:03 +0200

    FIX: animation on MenuItem hover effect

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const MenuItem = ({label}: Props) => {
   return (
-    <li className=' relative before:left-6 hover:before:left-0 before:transition-all before:duration-500 before:bg-white before:h-2 before:w-0 hover:before:w-12 before:rounded-md before:absolute before:-top-2 hover:before:-top-1 a flex h-full items-center'> 
+    <li className='relative before:left-[50%] hover:before:left-0 before:transition-all before:duration-500 before:bg-cyan-300 before:h-2 before:w-0 hover:before:w-[100%] before:rounded-md before:absolute before:-top-2 hover:before:-top-1 a flex h-full items-center'> 
         <p>{label}</p>
       </li>
   )
